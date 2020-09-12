69a2e23214d0fa7296fce5f2236c9821241161ba
Bryant Steven <stevenrincon_01@hotmail.com>
2020-09-12 10:45:11 -0500

    Profile.php creado css añadidos

--- profile.php
+++ profile.php
@@ -15,6 +15,21 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 -->
+<?php
+      session_start();
+      if(!isset($_SESSION['usuario'])){
+          echo '
+              <script>
+                  alert("Para acceder a este módulo, debes iniciar sesión");
+                  window.location= "login-register.php";
+              </script>
+          ';
+          session_destroy();
+          die();
+      }
+      //session_destrodfsfy();
+  ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 
