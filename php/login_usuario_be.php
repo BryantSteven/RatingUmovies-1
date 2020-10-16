2d08055ef4cd265b8a32c844fdf4eb91e48ae82e
Bryant Steven <stevenrincon_01@hotmail.com>
2020-10-16 11:52:47 -0500

    Actualicé credenciales para base de datos con Add-On de Jaws DB
    Se cambio el apuntador de direccion del servidor

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -1,30 +1,30 @@
 <?php
 
-    // session_start();
+    session_start();
 
-    // include 'conexion_be.php';
+    include 'conexion_be.php';
 
-    // $correo = $_POST['correo'];
-    // $contrasena = $_POST['contrasena'];
-    // $contrasena= hash('sha512', $contrasena);
+    $correo = $_POST['correo'];
+    $contrasena = $_POST['contrasena'];
+    $contrasena= hash('sha512', $contrasena);
 
-    // $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and contrasena ='$contrasena'");
+    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' and contrasena ='$contrasena'");
 
-    // if(mysqli_num_rows($validar_login) > 0 ){
-    //     $_SESSION['usuario'] = $correo;
-    //     header("location: ../index.html");
-    //     exit;
+    if(mysqli_num_rows($validar_login) > 0 ){
+        $_SESSION['usuario'] = $correo;
+        header("location: ../index.html");
+        exit;
             
         
-    // }else{
-    //     echo '
-    //         <script>
-    //             alert("Verifique las credenciales");
-    //             window.location="../login-register.php";
-    //         </script>
-    //     ';
-    //     exit;   
-    // }
+    }else{
+        echo '
+            <script>
+                alert("Verifique las credenciales");
+                window.location="../login-register.php";
+            </script>
+        ';
+        exit;   
+    }
 
 
 ?>
